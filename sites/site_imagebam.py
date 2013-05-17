ec32b84182fc658a5834f7068255329faf4658d1
4pr0n <1fakeyfake@gmail.com>
2013-05-17 07:09:55 -0700

    Expanded imagebam to get paginated galleries

--- sites/site_imagebam.py
+++ sites/site_imagebam.py
@@ -22,6 +22,7 @@ class imagebam(basesite):
 			if len(galleries) == 0 or '/gallery/' not in galleries[0]:
 				raise Exception("Required '/gallery/' not found")
 			url = galleries[0]
+		if not url.endswith('/'): url += '/'
 		return url
 
 	""" Discover directory path based on URL """
@@ -34,11 +35,23 @@ class imagebam(basesite):
 	def download(self):
 		self.init_dir()
 		r = self.web.get(self.url)
-		links = self.web.between(r, "href='http://www.imagebam.com/image/", "'")
-		for index, link in enumerate(links):
-			link = "http://www.imagebam.com/image/%s" % link
-			self.download_image(link, index + 1, total=len(links)) 
+		index = 0
+		total = 0
+		page = 1
+		while True:
+			links = self.web.between(r, "href='http://www.imagebam.com/image/", "'")
+			total += len(links)
+			for link in links:
+				index += 1
+				link = "http://www.imagebam.com/image/%s" % link
+				self.download_image(link, index, total=total) 
+				if self.hit_image_limit(): break
 			if self.hit_image_limit(): break
+			page += 1
+			if 'class="pagination_link">%d</a>' % page in r:
+				r = self.web.get('%s%d' % (self.url, page))
+			else:
+				break
 		self.wait_for_threads()
 	
 	def download_image(self, url, index, total):
