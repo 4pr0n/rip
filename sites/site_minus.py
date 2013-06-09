4b91d729b24201ffc3842c8d409d0182f5cb0d67
4pr0n <1fakeyfake@gmail.com>
2013-06-09 00:52:34 -0700

    Added support for minus guest albums

--- sites/site_minus.py
+++ sites/site_minus.py
@@ -9,29 +9,42 @@ class minus(basesite):
 	def sanitize_url(self, url):
 		if not 'minus.com' in url:
 			raise Exception('')
-		if not '.minus.com' in url or \
-				'i.minus.com' in url or \
-				'/.minus.com' in url or \
-				'www.minus.com' in url:
+		if 'i.minus.com' in url or \
+				'/.minus.com' in url:
 			raise Exception('required <b>http://&lt;user&gt;.minus.com/</b> not found in URL')
+		if not '.minus.com' in url or 'www.minus.com' in url:
+			temp = url.replace('http://', '').replace('https://', '')
+			if not '/' in temp or '/' in temp[temp.find('/')+1:]:
+				raise Exception('invalid minus url')
 		return url
 
 	""" Discover directory path based on URL """
 	def get_dir(self, url):
 		url = url.replace('http://', '').replace('https://', '')
-		url = url.replace('.minus.com', '').replace('/uploads', '')
-		dirs = url.split('/')
-		if '' in dirs: dirs.remove('')
-		return 'minus_%s' % '_'.join(dirs)
+		if '.minus.com' in url:
+			# Account upload
+			url = url.replace('.minus.com', '').replace('/uploads', '')
+			while url.endswith('/'): url = url[:-1]
+			dirs = url.split('/')
+			if '' in dirs: dirs.remove('')
+			return 'minus_%s' % '_'.join(dirs)
+		else:
+			# Guest upload
+			url = url[url.find('/')+1:]
+			return 'minus_guest_%s' % url
+			
 	
 	def download(self):
 		# Check URL, download all albums or just the URL
 		self.init_dir()
 		d = self.working_dir
-		dirs = d[d.find('minus_')+len('minus_'):].split('_')
+		dirs = d.split('_')[1:] # d[d.find('minus_')+len('minus_'):].split('_')
+		self.debug('dirs: %s' % str(dirs))
 		if len(dirs) == 1:
 			# Account
+			self.debug('account')
 			user = dirs[0]
+			# Albums
 			url = 'http://%s.minus.com/uploads' % user
 			r = self.web.get(url)
 			albumids = self.web.between(r, '"reader_id": "', '"')
@@ -42,8 +55,31 @@ class minus(basesite):
 				self.download_album(suburl)
 				self.wait_for_threads()
 				self.working_dir = prev_dir
+			'''
+			# photos
+			url = 'http://%s.minus.com/' % user
+			r = self.web.get(url)
+			imageids = self.web.between(r, '<img src="', '"')
+			i = 0
+			while i < len(imageids):
+				if not 'minus.com' in imageids[i] or imageids[i].count('/') > 3:
+					imageids.pop(i)
+				else:
+					i += 1
+			for index, imageid in enumerate(imageids):
+				url = 'http://i%s' % imageid[imageid.find('.'):]
+				url = url[:url.find('_')] + url[url.rfind('.'):]
+				self.download_image(url, index + 1, total=len(imageids))
+			'''
+			
+		elif dirs[0] == 'guest':
+			# Guest
+			self.debug('guest')
+			url = 'http://minus.com/%s' % (dirs[1])
+			self.download_album(url)
 		elif len(dirs) == 2:
-			# Album
+			# Account album
+			self.debug('account album')
 			url = 'http://%s.minus.com/%s' % (dirs[0], dirs[1])
 			self.download_album(url)
 		else:
