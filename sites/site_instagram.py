30a65850b4d10ae403d930a80751d647fbbdd3a4
4pr0n <1fakeyfake@gmail.com>
2013-09-02 16:53:01 -0700

    instagram ripper now uses instagram.com

--- sites/site_instagram.py
+++ sites/site_instagram.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from basesite import basesite
+from json import loads
+from time import sleep
 
 """
 	Downloads instagram albums
@@ -9,60 +11,59 @@ class instagram(basesite):
 	
 	""" Parse/strip URL to acceptable format """
 	def sanitize_url(self, url):
-		if'web.stagram.com/n/' in url:
+		if'instagram.com/' in url:
 			# Legit
 			pass
-		elif 'instagram.com/' in url:
+		elif 'web.stagram.com/n/' in url:
 			# Convert to web.stagram
-			user = url[url.find('.com/')+len('.com/'):]
+			user = url[url.find('.com/n/')+len('.com/n/'):]
 			if '/' in user: user = user[:user.find('/')]
-			url = 'http://web.stagram.com/n/%s' % user
+			url = 'http://instagram.com/%s' % user
 		else:
 			raise Exception('')
 		if '?' in url: url = url[:url.find('?')]
 		if '#' in url: url = url[:url.find('#')]
+		while url.endswith('/'): url = url[:-1]
 		return url
 
 	""" Discover directory path based on URL """
 	def get_dir(self, url):
-		user = url[url.find('.com/n/')+len('.com/n/'):]
-		if '/' in user: user = user[:user.find('/')]
+		user = url[url.rfind('/')+1:]
 		return 'instagram_%s' % user
 
 	def download(self):
 		self.init_dir()
-		r = self.web.get(self.url)
-		totals = self.web.between(r, 'font-size:123.1%;">', '<')
-		if len(totals) > 0: total = int(totals[1])
-		else: total = -1
+		baseurl = '%s/media' % self.url
+		url = baseurl
 		index = 0
 		while True:
-			chunks = self.web.between(r, '<div class="infolist">', '<div class="like_comment')
-			for chunk in chunks:
-				imgs = self.web.between(chunk, '<a href="', '"')
-				if len(imgs) < 4: continue
-				img = imgs[3].replace('_6.', '_7.')
-				self.debug('found img: %s' % img)
-				if '<div class="hasvideo' in chunk:
-					vid = img.replace('_7.jpg', '_101.mp4')
-					self.debug('video found, url: %s' % vid)
-					meta = self.web.get_meta(vid)
-					if 'Content-Length' in meta and meta['Content-Length'] != '0' and \
-						'Content-Type' in meta and 'video' in meta['Content-Type']:
-						self.debug('meta shows vid is legit')
-						img = vid
+			self.debug('loading %s' % url)
+			r = self.web.get(url)
+			try: json = loads(r)
+			except:
+				self.wait_for_threads()
+				self.debug('invalid json response:\n%s' % r)
+				raise Exception('unable to parse json at %s' % url)
+			if not json['status'] == 'ok':
+				self.wait_for_threads()
+				self.log('status NOT OK: %s' % json['status'])
+				raise Exception('status not "ok": %s' % json['status'])
+			last_id = 0
+			for item in json['items']:
+				last_id = item['id']
+				for media_type in ['videos', 'images']:
+					if not media_type in item: continue
+					index += 1
+					media_url = item[media_type]['standard_resolution']['url']
+					if self.urls_only:
+						self.add_url(index, media_url)
 					else:
-						self.debug('meta shows vid is not legit')
-				
-				index += 1
-				if self.urls_only:
-					self.add_url(index, img, total=total)
-				else:
-					self.download_image(img, index, total=total) 
-					if self.hit_image_limit(): break
-			if self.hit_image_limit(): break
-			earliers = self.web.between(r, ' [ <a href="/n/', '"')
-			if len(earliers) != 2: break
-			r = self.web.get('http://web.stagram.com/n/%s' % earliers[0])
+						self.download_image(media_url, index)
+						sleep(0.5)
+					break
+				if self.hit_image_limit(): break
+			if not json['more_available'] or last_id == 0: break
+			sleep(2)
+			url = '%s?max_id=%s' % (baseurl, last_id)
 		self.wait_for_threads()
 	
