7aa0429137db63f0c8a3ccc3623a7093a518a1ba
4pr0n <1fakeyfake@gmail.com>
2013-09-28 21:35:58 -0700

    formatting graph so it's prettier

--- graphs/script.js
+++ graphs/script.js
@@ -42,27 +42,75 @@ function showGraph($t) {
 				$t.find('div.graph')
 					.slideDown()
 					.highcharts({
+						colors: [ '#f00', '#0f0', '#00f', '#ff0', '#f0f', '#0ff'],
 						chart: {
-							type: 'line'
+							type: 'line',
+							borderRadius:    '20px',
+							backgroundColor: 'rgba(255, 255, 255, 0.0)',
+							plotBackgroundColor: 'rgba(255, 255, 255, 0.0)',
+							style: {
+								fontFamily: 'Verdana, monospace',
+								fontSize: '1.2em',
+								color: '#fff',
+							},
+						},
+						legend: {
+							itemStyle: {
+								color: '#ddd',
+							},
+							itemHoverStyle: {
+								color: '#fff',
+							}
 						},
 						title: {
-							text: json.title,
+							text: null, //json.title,
+							style: {
+								fontFamily: 'Verdana, monospace',
+								fontSize: '1.4em',
+								color: '#eee',
+							},
 						},
 						xAxis: {
 							type: 'datetime',
+							labels: {
+								style: {
+									color: '#eee',
+								},
+							},
 						},
 						yAxis: [{
 							min: 0,
 							title: null,
+							labels: {
+								style: {
+									color: '#eee',
+								},
+							},
 						},{
 							min: 0,
 							opposite: true,
 							title: null,
+							labels: {
+								style: {
+									color: '#eee',
+								},
+							},
 						}],
 						plotOptions: {
 							series: {
 								pointStart: json.pointStart,
 								pointInterval: json.pointInterval,
+								style: {
+									backgroundColor: '#888',
+								},
+							},
+						},
+						credits: {
+							enabled: true,
+							text: 'all times in PST',
+							href: null,
+							style: {
+								color: '#eee',
 							},
 						},
 						series: json.series,
